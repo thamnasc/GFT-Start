aee4c19001f0ff3f3754a30a5c8a353bf596ad2a
thamnasc <thalitanas09@gmail.com>
2022-02-12 23:10:02 -0300

    desafio: exibindo números pares

---
+++ GFT Start #4 Java/4. Desafios/Exibindo Números Pares/src/com/dio/Main.java
@@ -0,0 +1,23 @@
+package com.dio;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        //instanciando Scanner
+        Scanner scan = new Scanner(System.in);
+
+        int N;
+
+        //entrada de dados
+        N = scan.nextInt();
+
+        for(int i = 2; i <= N; i++) {
+            //verifica se o número é par
+            if(i % 2 == 0) System.out.println(i);
+        }
+    }
+}
