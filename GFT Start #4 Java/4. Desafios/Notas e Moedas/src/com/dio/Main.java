6a551a8d1e4aa3204087a964f5f494b8a8f6ac46
thamnasc <thalitanas09@gmail.com>
2022-03-03 00:12:42 -0300

    desafio notas e moedas

---
+++ GFT Start #4 Java/4. Desafios/Notas e Moedas/src/com/dio/Main.java
@@ -0,0 +1,38 @@
+package com.dio;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        double N;
+        int quociente, reais;
+        int[] notas = {100,50,20,10,5,2};
+        int[] moedasInt = {100,50,25,10,5,1};
+
+        N = sc.nextDouble();
+        reais = (int) (N * 100);
+
+        System.out.println("NOTAS:");
+
+        for (int nota : notas) {
+            quociente = reais / (nota * 100);
+            reais %= (nota * 100);
+            System.out.println(quociente + " nota(s) de R$ " + nota + ".00");
+        }
+
+        System.out.println("MOEDAS:");
+
+        for (int moeda : moedasInt) {
+            quociente = reais / (moeda);
+            reais %= (moeda);
+            System.out.print(quociente + " moeda(s) de R$ ");
+            System.out.printf("%.2f\n", (float) moeda/100);
+        }
+
+        sc.close();
+    }
+}
